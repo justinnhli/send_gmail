ac17b1a3b2d2f567dfb4dec6f1583fa3b1ccec6f
Justin Li <justinnhli@gmail.com>
2018-10-14 23:57:29 -0700

    switch to google-auth
    oauth2client has been deprecated; see https://google-auth.readthedocs.io/en/latest/oauth2client-deprecation.html

--- send_gmail.py
+++ send_gmail.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
+import webbrowser
 from base64 import urlsafe_b64encode
+from functools import lru_cache
 from email.mime.application import MIMEApplication
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
@@ -8,13 +10,10 @@ from os.path import join as join_path, basename, realpath, expanduser, dirname
 
 from apiclient import discovery, errors
 from commonmark import Parser as CommonMarkParser, HtmlRenderer
-from httplib2 import Http
+from google_auth_oauthlib.flow import Flow
 from jinja2 import Environment as JinjaEnvironment
-from oauth2client import client, tools
-from oauth2client.file import Storage
 
 REAL_PATH = realpath(expanduser(dirname(__file__)))
-CREDENTIAL_PATH = join_path(REAL_PATH, 'stored-credentials.json')
 CLIENT_SECRET_FILE = join_path(REAL_PATH, 'client_secret.json')
 
 # If modifying these scopes, delete your previously saved credentials
@@ -22,48 +21,49 @@ SCOPES = [
     'https://www.googleapis.com/auth/gmail.modify',
     'https://www.googleapis.com/auth/gmail.compose',
 ]
-APPLICATION_NAME = 'Python Gmail Sender'
 
 
+@lru_cache()
 def get_credentials():
-    """Get valid user credentials from storage, or create it.
+    # Create the flow using the client secrets file from the Google API Console.
+    flow = Flow.from_client_secrets_file(
+        CLIENT_SECRET_FILE,
+        scopes=SCOPES,
+        redirect_uri='urn:ietf:wg:oauth:2.0:oob', # FIXME this should be read from client secret as well
+    )
 
-    If nothing has been stored, or if the stored credentials are invalid,
-    the OAuth2 flow is completed to obtain the new credentials.
+    # Tell the user to go to the authorization URL.
+    auth_url, _ = flow.authorization_url(prompt='consent')
 
-    Returns:
-        Credentials, the obtained credential.
-    """
-    store = Storage(CREDENTIAL_PATH)
-    credentials = store.get()
-    if not credentials or credentials.invalid:
-        flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
-        flow.user_agent = APPLICATION_NAME
-        credentials = tools.run_flow(flow, store)
-        print('Storing credentials to ' + CREDENTIAL_PATH)
-    return credentials
+    webbrowser.open(auth_url)
+
+    # The user will get an authorization code. This code is used to get the
+    # access token.
+    code = input('Enter the authorization code: ')
+    flow.fetch_token(code=code)
+
+    return flow.credentials
 
 
 def get_service():
-    """Get a Service object for Gmail.
+    """Build a Gmail API service.
 
     Returns:
-        A valid Service object.
+        Resource: The Resource for interacting with a Google API.
     """
     credentials = get_credentials()
-    http = credentials.authorize(Http())
-    return discovery.build('gmail', 'v1', http=http)
+    return discovery.build('gmail', 'v1', credentials=credentials)
 
 
-def create_message(to, subject, message_text, html=True, attachments=None):
-    """Create a message for an email, using the low-level API
+def create_message(address, subject, message_text, html=True, attachments=None):
+    """Create a message for an email, using the low-level API.
 
     Arguments:
-        to (str): Email address of the receiver.
+        address (str): Email address(es) of the receiver.
         subject (str): The subject of the email message.
         message_text (str): The text of the email message.
         html (bool): If True, treat message as HTML instead of plain text. Defaults to True.
-        attachments ([str]): A list of filepaths to attach to the email. Defaults to [].
+        attachments (List[str]): A list of filepaths to attach to the email. Defaults to [].
 
     Returns:
         dict: A base64url encoded email JSON "object".
@@ -73,7 +73,7 @@ def create_message(to, subject, message_text, html=True, attachments=None):
 
     # create the initial message
     message = MIMEMultipart()
-    message['to'] = to
+    message['to'] = address
     message['subject'] = subject
 
     # add the email body text
@@ -100,49 +100,53 @@ def send_message(service, user_id, message):
         service (Service): Authorized Gmail API service instance.
         user_id (str): User's email address. The special value "me"
             can be used to indicate the authenticated user.
-            message: Message to be sent.
+        message (str): Message to be sent.
 
     Returns:
-        Sent Message.
+        dict: The sent message as a JSON-like object.
     """
     try:
-        message = (service.users().messages().send(userId=user_id, body=message).execute())
+        message = service.users().messages().send(userId=user_id, body=message).execute()
         print('Message sent; id={}'.format(message['id']))
         return message
     except errors.HttpError as error:
         print('An error occurred: {}'.format(error))
 
 
-def send_email(to, subject, body, html=True, attachments=None):
+def send_email(address, subject, body, html=True, attachments=None):
     """Send an email.
 
     Arguments:
-        to (str): Email address of the receiver.
+        address (Union[Seq[str], str]): Email address of the receiver.
         subject (str): The subject of the email message.
         body (str): The text of the email message.
         html (bool): If True, treat message as HTML instead of plain text. Defaults to True.
-        attachments ([str]): A list of filepaths to attach to the email. Defaults to [].
+        attachments (List[str]): A list of filepaths to attach to the email. Defaults to [].
 
     Returns:
-        Sent Message.
+        Message: The sent message.
     """
+    if isinstance(address, str):
+        address = [address]
+    assert hasattr(address, '__iter__')
     if attachments is None:
         attachments = []
     service = get_service()
-    message = create_message(to=to, subject=subject, message_text=body, attachments=attachments, html=html)
+    address = ', '.join(address)
+    message = create_message(address=address, subject=subject, message_text=body, attachments=attachments, html=html)
     return send_message(service, 'me', message)
 
 
-def markdown_render(md):
+def markdown_render(markdown):
     """Convert Markdown to HTML.
 
     Arguments:
-        md (str): The Markdown document.
+        markdown (str): The Markdown document.
 
     Returns:
         str: The rendered HTML.
     """
-    return HtmlRenderer().render(CommonMarkParser().parse(md))
+    return HtmlRenderer().render(CommonMarkParser().parse(markdown))
 
 
 def jinja_render(template, context):
@@ -150,7 +154,7 @@ def jinja_render(template, context):
 
     Arguments:
         template (str): The Jinja template string.
-        context ({str: *}): A dictionary of variables for Jinja.
+        context (Dict[str, obj]): A dictionary of variables for Jinja.
 
     Returns:
         str: The rendered string.
@@ -162,13 +166,13 @@ def main():
     """Send an email from the command line."""
     from argparse import ArgumentParser
     arg_parser = ArgumentParser()
-    arg_parser.add_argument('to', metavar='TO', help='Email address of the receiver')
+    arg_parser.add_argument('addresses', metavar='TO', help='(Comma-separated) email address(es) of the recipient(s)')
     arg_parser.add_argument('subject', metavar='SUBJECT', help='Subject of the email')
     arg_parser.add_argument('body', metavar='BODY', help='body of the email')
     arg_parser.add_argument('--html', action='store_true', help='treat body as HTML (default False)')
     args = arg_parser.parse_args()
     send_email(
-        to=args.to,
+        address=args.to,
         subject=args.subject,
         body=args.body,
         html=args.html,
